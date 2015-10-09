a24e8737b825607c327c94b35a27693b4d24a99a
debankar <anantasahu2014@gmail.com>
2015-10-09 19:03:07 +0530

    it is my 1st commit

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author ananta
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi sahu,how are you");
+	}
+
+}
