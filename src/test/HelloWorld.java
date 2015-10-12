bb2a3bd11ab551787996561266287cd50b822f4b
debankar <anantasahu2014@gmail.com>
2015-10-12 07:03:24 +0530

    11102015

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -15,18 +15,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi sahu,how are you");
-		System.out.println("hi darling");
-		System.out.println("hi,this is deepak");
-		System.out.println("Hi,this is depti.....my 4th change");
-		System.out.println("ananta");
-		System.out.println("babu");
-		System.out.println("helllllllllllllllllllllllloooooooo");
-		System.out.println("wewerwerwerwerwerwerrwe");
-		System.out.println("hhhhhhhhhhhhhhh");
-		System.out.println("email test");
-		System.out.println("hi rubi darling");
-		System.out.println("hello Babureeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-		System.out.println("222222222222222222222221111111112");
+		
 	}
 
 }
